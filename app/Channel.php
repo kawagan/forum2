e5c880f0008a13783203a699635f0b6afa354df5
kawagan <kawajan2@hotmail.com>
2018-01-20 23:22:25 +0100

    add $guarded

--- app/Channel.php
+++ app/Channel.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
+    protected $guarded=[];
+    
     public function threads()
     {
         return $this->hasMany('App\Thread');
