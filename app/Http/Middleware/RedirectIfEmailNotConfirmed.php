9857f8666d63f72a7c00897d8d523c4711d080bc
kawagan <kawajan2@hotmail.com>
2018-01-20 23:24:09 +0100

    middleware admin

--- app/Http/Middleware/RedirectIfEmailNotConfirmed.php
+++ app/Http/Middleware/RedirectIfEmailNotConfirmed.php
@@ -6,22 +6,24 @@ use Closure;
 
 class RedirectIfEmailNotConfirmed
 {
-    /**
+     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     // the user can't make thread without confirm email
     // we make it as middlware becs we maybe need it also in many places
     public function handle($request, Closure $next)
     {
-        if( !$request->user()->confirmed){
-            session()->push('m','warning');
-            session()->push('m','you are not confirmed your E-mail.');
-            return redirect()->back();
-        }
+            $user=$request->user();
+            
+            if(!$user->confirmed && !$user->isAdmin()){
+                session()->push('m','warning');
+                session()->push('m','you are not confirmed your E-mail.');
+                return redirect()->back();
+            }
         return $next($request);
     }
 }
